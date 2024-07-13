dae5a9241b94e1154e4200041dc2276db2d9ca25
viniciochavess <viniciochavess@gmail.com>
2024-07-13 16:33:07 -0300

    Create Repositories combustivel

---
+++ src/main/java/com/gestao_combustivel/gestao_combustivel/Modules/Combustivel/Repositories/CombustivelRepository.java
@@ -0,0 +1,13 @@
+package com.gestao_combustivel.gestao_combustivel.Modules.Combustivel.Repositories;
+
+import java.util.Optional;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.gestao_combustivel.gestao_combustivel.Modules.Combustivel.CombustivelEntity;
+
+public interface CombustivelRepository extends JpaRepository<CombustivelEntity, Long> {
+
+    Optional<CombustivelEntity> findByFuel(int fuel);
+
+}
