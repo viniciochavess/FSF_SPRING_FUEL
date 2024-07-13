32b6c0fbe150b24fe1106bf075245e0c135381f8
viniciochavess <viniciochavess@gmail.com>
2024-07-13 17:13:34 -0300

    Create Repository MovementFuel

---
+++ src/main/java/com/gestao_combustivel/gestao_combustivel/Modules/Combustivel/Entity/MovementFuel/Repositories/MovementFuelRepository.java
@@ -0,0 +1,14 @@
+package com.gestao_combustivel.gestao_combustivel.Modules.Combustivel.Entity.MovementFuel.Repositories;
+
+import java.util.Optional;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.gestao_combustivel.gestao_combustivel.Modules.Combustivel.Fuel;
+import com.gestao_combustivel.gestao_combustivel.Modules.Combustivel.Entity.MovementFuel.MovementFuelEntity;
+
+public interface MovementFuelRepository extends JpaRepository<MovementFuelEntity, Long> {
+
+    Optional<MovementFuelEntity> findByFuel(Fuel fuel);
+
+}
