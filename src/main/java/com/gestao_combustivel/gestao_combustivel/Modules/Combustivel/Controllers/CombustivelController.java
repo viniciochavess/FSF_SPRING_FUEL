82044072700c823d474d6ecf1cd4d1ebe0a42cec
viniciochavess <viniciochavess@gmail.com>
2024-07-14 17:05:08 -0300

    return all fuel register

--- src/main/java/com/gestao_combustivel/gestao_combustivel/Modules/Combustivel/Controllers/CombustivelController.java
+++ src/main/java/com/gestao_combustivel/gestao_combustivel/Modules/Combustivel/Controllers/CombustivelController.java
@@ -1,13 +1,17 @@
 package com.gestao_combustivel.gestao_combustivel.Modules.Combustivel.Controllers;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import com.gestao_combustivel.gestao_combustivel.Modules.Combustivel.CombustivelEntity;
+import com.gestao_combustivel.gestao_combustivel.Modules.Combustivel.Repositories.CombustivelRepository;
 import com.gestao_combustivel.gestao_combustivel.Modules.Combustivel.UseCase.CreateCombustivelUseCase;
 
 import jakarta.validation.Valid;
@@ -15,6 +19,9 @@ import jakarta.validation.Valid;
 @Controller
 @RequestMapping("/combustivel")
 public class CombustivelController {
+
+    @Autowired
+    private CombustivelRepository combustivelRepository;
 
     @Autowired
     private CreateCombustivelUseCase createCombustivelUseCase;
@@ -27,6 +34,12 @@ public class CombustivelController {
         } catch (Exception e) {
             return ResponseEntity.badRequest().body(e.getMessage());
         }
+    }
+
+    @GetMapping("")
+    public ResponseEntity<List<CombustivelEntity>> GetAll() {
+        List<CombustivelEntity> result = combustivelRepository.findAll();
+        return ResponseEntity.ok().body(result);
     }
 
 }
