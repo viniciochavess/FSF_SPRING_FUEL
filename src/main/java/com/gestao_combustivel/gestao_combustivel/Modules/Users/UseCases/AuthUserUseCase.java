189d75cf5e0e1643fd25595c64ba64da7dadb88e
viniciochavess <viniciochavess@gmail.com>
2024-07-15 20:18:09 -0300

    Password hash

---
+++ src/main/java/com/gestao_combustivel/gestao_combustivel/Modules/Users/UseCases/AuthUserUseCase.java
@@ -0,0 +1,8 @@
+package com.gestao_combustivel.gestao_combustivel.Modules.Users.UseCases;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class AuthUserUseCase {
+    
+}
--- src/main/java/com/gestao_combustivel/gestao_combustivel/Modules/Users/UseCases/CreateUserUseCase.java
+++ src/main/java/com/gestao_combustivel/gestao_combustivel/Modules/Users/UseCases/CreateUserUseCase.java
@@ -1,6 +1,7 @@
 package com.gestao_combustivel.gestao_combustivel.Modules.Users.UseCases;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
 import com.gestao_combustivel.gestao_combustivel.Modules.Users.UserEntity;
@@ -13,11 +14,16 @@ public class CreateUserUseCase {
     @Autowired
     private UserRepository userRepository;
 
+    @Autowired
+    private PasswordEncoder passwordEncoder;
+
     public UserEntity execute(UserEntity userEntity) {
         this.userRepository.findByUsername(userEntity.getUsername())
                 .ifPresent((user) -> {
                     throw new UserFoundException();
                 });
+        var passwordHash = passwordEncoder.encode(userEntity.getPassword());
+        userEntity.setPassword(passwordHash);
         return this.userRepository.save(userEntity);
     }
 
