33e998c061aa8babf648d4fcf21bd3b2a5f2a8bf
viniciochavess <viniciochavess@gmail.com>
2024-07-15 20:58:41 -0300

    imports remove

--- src/main/java/com/gestao_combustivel/gestao_combustivel/Modules/Users/Controllers/AuthUserController.java
+++ src/main/java/com/gestao_combustivel/gestao_combustivel/Modules/Users/Controllers/AuthUserController.java
@@ -1,10 +1,8 @@
 package com.gestao_combustivel.gestao_combustivel.Modules.Users.Controllers;
 
-import javax.security.sasl.AuthenticationException;
-
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
-import org.springframework.http.HttpStatusCode;
+
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
