aeeda10be9f6e8505f849d5ecfb9c9d25e3998c9
viniciochavess <viniciochavess@gmail.com>
2024-07-15 20:52:58 -0300

    return auth entity response

--- src/main/java/com/gestao_combustivel/gestao_combustivel/Modules/Users/Controllers/AuthUserController.java
+++ src/main/java/com/gestao_combustivel/gestao_combustivel/Modules/Users/Controllers/AuthUserController.java
@@ -3,6 +3,9 @@ package com.gestao_combustivel.gestao_combustivel.Modules.Users.Controllers;
 import javax.security.sasl.AuthenticationException;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.HttpStatusCode;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -19,9 +22,13 @@ public class AuthUserController {
     private AuthUserUseCase authUserUseCase;
 
     @PostMapping
-    public String session(@RequestBody AuthUserDTO authUserDTO) throws AuthenticationException {
-        var result = this.authUserUseCase.execute(authUserDTO);
-        return result;
+    public ResponseEntity<Object> session(@RequestBody AuthUserDTO authUserDTO) {
+        try {
+            var result = this.authUserUseCase.execute(authUserDTO);
+            return ResponseEntity.ok().body(result);
+        } catch (Exception e) {
+            return ResponseEntity.status(HttpStatus.UNAUTHORIZED).body(e.getMessage());
+        }
 
     }
 
