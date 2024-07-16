c8b21d639f4698f45d4186462bb40b7e8346e03d
viniciochavess <viniciochavess@gmail.com>
2024-07-15 22:47:45 -0300

    Authorization Token

--- src/main/java/com/gestao_combustivel/gestao_combustivel/security/SecurityConfig.java
+++ src/main/java/com/gestao_combustivel/gestao_combustivel/security/SecurityConfig.java
@@ -1,14 +1,19 @@
 package com.gestao_combustivel.gestao_combustivel.security;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.www.BasicAuthenticationFilter;
 
 @Configuration
 public class SecurityConfig {
+    @Autowired
+    private SecurityFilter securityFilter;
+
     @Bean
     SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
         http.csrf(csrf -> csrf.disable())
@@ -16,7 +21,9 @@ public class SecurityConfig {
                     auth.requestMatchers("/usuarios").permitAll()
                             .requestMatchers("/session").permitAll();
                     auth.anyRequest().authenticated();
-                });
+                })
+                .addFilterBefore(securityFilter, BasicAuthenticationFilter.class);
+
         return http.build();
     }
 
