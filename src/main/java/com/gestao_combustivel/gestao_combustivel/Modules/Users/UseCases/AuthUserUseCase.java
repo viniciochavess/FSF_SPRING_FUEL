5c9f8bc2ad53c280a442174ace8ba67d029bc991
viniciochavess <viniciochavess@gmail.com>
2024-07-15 22:22:35 -0300

    expiração do token

--- src/main/java/com/gestao_combustivel/gestao_combustivel/Modules/Users/UseCases/AuthUserUseCase.java
+++ src/main/java/com/gestao_combustivel/gestao_combustivel/Modules/Users/UseCases/AuthUserUseCase.java
@@ -1,5 +1,8 @@
 package com.gestao_combustivel.gestao_combustivel.Modules.Users.UseCases;
 
+import java.time.Duration;
+import java.time.Instant;
+
 import javax.security.sasl.AuthenticationException;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -12,6 +15,7 @@ import com.auth0.jwt.JWT;
 import com.auth0.jwt.algorithms.Algorithm;
 import com.gestao_combustivel.gestao_combustivel.Modules.Users.Repository.UserRepository;
 import com.gestao_combustivel.gestao_combustivel.Modules.Users.dto.AuthUserDTO;
+
 
 @Service
 public class AuthUserUseCase {
@@ -36,7 +40,7 @@ public class AuthUserUseCase {
             throw new AuthenticationException();
         }
         Algorithm algorithm = Algorithm.HMAC256(secretKey);
-        var token = JWT.create().withIssuer("posto")
+        var token = JWT.create().withIssuer("posto").withExpiresAt(Instant.now().plus(Duration.ofHours(2)))
                 .withSubject(result.getId().toString())
                 .sign(algorithm);
         return token;
